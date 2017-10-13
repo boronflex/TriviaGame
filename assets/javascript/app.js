eadb61b5faeda88bcdb4d710b44898f7fd217e9f
boronflex <kyle_palmer@icloud.com>
2017-10-13 16:30:43 -0500

    built html skeleton with all basic elements, outlined needs in javascript file

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+//TODO:
+
+//get timer working counting down and displaying in the span set up html
+	//will need a separate time or a method to use the same timer to count between questions
+	//refer to stopwatch exercise to help set up the timer
+
+//build an object as a dictionary of questions and answers
+	//need to figure out how to mark correct answers
+	//pull questions randomly out of dictionary keys and answers out of the list values
+	//apply to question html and buttons
+		//figure out how to get answers to go to random buttons
+
+
+//get user input from buttons
+	//when question is answered correctly store +1 to correct answers
+	//when question is answered incorrectly store +1 to incorrect answers
+
+//set up screen for correct answer
+//set up screen for incorrect answer
+//set up screen for time running out
+	//needs to cover up main answer div
+	//possibly other hidden divs that cover up the main one
+	//could use position relative an z score to set up
+
+
+
+
+//instructions for building advanced game
+// * You'll create a trivia game that shows only one question until the player
+// answers it or their time runs out.
+
+// * If the player selects the correct answer, show a screen congratulating them
+// for choosing the right option. After a few seconds, display the next question
+// -- do this without user input.
+
+// * The scenario is similar for wrong answers and time-outs.
+
+//   * If the player runs out of time, tell the player that time's up and
+// display the correct answer. Wait a few seconds, then show the next question.
+//   * If the player chooses the wrong answer, tell the player they selected the
+// wrong option and then display the correct answer. Wait a few seconds, then show
+// the next question.
+
+// * On the final screen, show the number of correct answers, incorrect answers,
+// and an option to restart the game (without reloading the page).

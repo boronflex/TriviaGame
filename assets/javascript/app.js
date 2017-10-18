a6359ded455f6fa4188f6e23e6878db9d2ec4997
boronflex <kyle_palmer@icloud.com>
2017-10-18 00:21:44 -0500

    game mechanics 100%, needs design work and populated questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,14 @@
-$(document).click(function(){
-  $("#clickanywhere").hide();
-  gameLogic.questionAnswerTimer();
-  // view.displayQuestionAnswers(); // this is still in the wrong place
+
+$("#click-to-start").click(function(){
+	$("#click-to-start").hide();
+  	view.displayQuestionAnswers();
+  	gameLogic.questionAnswerTimer();
 });
 
-$(".answer-button").click(function(){
-	getAnswer();
-});
 
-// view.displayQuestionAnswers();//this is out of place
-	
-
-//TODO:
-
-//get timer working counting down and displaying in the span set up html
-	//will need a separate time or a method to use the same timer to count between questions
-	//refer to stopwatch exercise to help set up the timer
 var gameLogic = {
 	
 	timerDisplay: 10,
-
-	// breakTimer: 5,
-
-	isQuestion: true,
 
 	clockRunning: false,//copied from stopwatch activity
 
@@ -33,7 +19,7 @@ var gameLogic = {
 	questionAnswerTimer: function(){
 
 		//if the its in a question portion
-		//call question timer
+		//call question timer	
 
 		if (!gameLogic.clockRunning) {//copied from stopwatch activity
 	        
@@ -41,15 +27,6 @@ var gameLogic = {
 	        gameLogic.clockRunning = true;
 	    }
 
-	    //if its in the break portion
-	    //call break timer
-
-	    // if (!gameLogic.clockRunning){
-	    // 	gameLogic.intervalQuestion = setInterval(gameLogic.decrementBreak, 1000);
-	    //     gameLogic.clockRunning = true;
-	    // }
-	    // //
-		
 	},
 
 	decrementQuestion: function(){
@@ -72,6 +49,8 @@ var gameLogic = {
 					
 			if (gameLogic.timerDisplay === 0){
 
+				handler.answerClicked = false;
+				gameLogic.wrongGuesses += 1;
 				gameLogic.answered = true;
 				gameLogic.clockRunning = false;
 				gameLogic.timerDisplay = 5;
@@ -92,46 +71,41 @@ var gameLogic = {
 			$("#time-remaining-number").text(gameLogic.timerDisplay);
 
 			if (gameLogic.timerDisplay === 0){
-				
+
+				handler.answerClicked = false;				
 				gameLogic.timerDisplay = 10;
 				gameLogic.currentQuestion += 1; //move to the next question
 				gameLogic.answered = false;
 				gameLogic.clockRunning = false;
 
 				clearInterval(gameLogic.intervalQuestion);
+
+				if (gameLogic.correctGuesses + gameLogic.wrongGuesses !==  gameLogic.questionBank.length){
+					
+					view.displayQuestionAnswers();//once the inbetween timer runs out grab a new question
+					gameLogic.questionAnswerTimer();
+					view.closeAllModals();
+
+				} else {
+					handler.answerClicked = true;
+					clearInterval(gameLogic.intervalQuestion);
+					gameLogic.clockRunning = false;
+					view.gameOverScreen();	
+				}
 				
-				view.displayQuestionAnswers();//once the inbetween timer runs out grab a new question
-				gameLogic.questionAnswerTimer();
-				view.closeAllModals();
 			}
 
 		}
 		
 	},
 
-
-//build an object as a dictionary of questions and answers
-//need list of objects here
-
-//random number to retrive question-->
-// var numberMe = 1; -randomnumber here
-//questionBank[1].question = "the question1" -----to call questions
-//questionBank[1].answers[0] = "answer1" -----to call answers
-
-
-//possibly don't need the correct key: value par, just put the correct answer first
-//and the junk answers after, the question answer bank will remain unchanged so 
-//if the button clicked's value doesnt equal answers[0], it will be wrong
-//random sorting won't interfere with this
-
-//question bank, this looks pretty good- happy with this
 	questionBank: [
 
 		{question: "the question0",
 		answers: ["right","answer2","answer3","answer4"]},
 
 		{question: "the question1",
-		answers: ["right","answer2","answer3","answer4"]},
+		answers: ["right","answer2","answer3","answer4"]}
 
 		{question: "the question2",
 		answers: ["right","answer2","answer3","answer4"]},
@@ -159,7 +133,6 @@ var gameLogic = {
 //instructions don't specify random questions, but the answers should be mixed up
 
 	currentQuestion: 0,
-
 	correctGuesses: 0,
 	wrongGuesses: 0,
 
@@ -188,50 +161,30 @@ var gameLogic = {
 }
 
 var view = {
-	//set up screen for correct answer
-	//set up screen for incorrect answer
-	//set up screen for time running out
-	//needs to cover up main answer div maybe just use modal
-	// jquery hide and show methods will probably work here
-	//possibly other hidden divs that cover up the main one
-	//could use position relative an z score to set up
 
-	//method on modal from w3schools adapted to jquery
+	//should probably put all these modals on one method in a list
+	//but i need to go ahead and get this out
 
+	clickToStartScreen: function(){
 
-	//use to handle wrong, right, time out, game over
-	//could probably use list of functions here but there were context problems
-	//with he question bank
+		$("#click-to-start").css("display","block");
+	},
+
 	correctAnswerScreen: function(){
-		// if(gameLogic.isQuestion === false){
-			$("#correct-answer-screen").css("display","block");
-		// } else {
-		// 	$("#correct-answer-screen").css("display","none");
-		// }	
+		$("#correct-answer-screen").css("display","block");
 	},
 	wrongAnswerScreen: function(){
-		// if(gameLogic.isQuestion === false){
-			$("#incorrect-answer-screen").css("display","block");
-		// } else {
-		// 	$("#incorrect-answer-screen").css("display","none");
-		// }
-		
+		$("#incorrect-answer-screen").css("display","block");
+		$(".show-correct-answer").text(gameLogic.questionBank[gameLogic.currentQuestion].answers[0])	
 	},
 	timeUpScreen: function(){
-		// if(gameLogic.isQuestion === false){
-			$("#time-runout-screen").css("display","block");
-		// } else {
-		// 	$("#time-runout-screen").css("display","none");
-		// }
-		
+		$("#time-runout-screen").css("display","block");
+		$(".show-correct-answer").text(gameLogic.questionBank[gameLogic.currentQuestion].answers[0])
 	},
 	gameOverScreen: function(){
-		// if(gameLogic.isQuestion === false){
-			$("#game-complete-screen").css("display","block");
-		// } else {
-		// 	$("#game-complete-screen").css("display","none");
-		// }
-		
+		$("#game-complete-screen").css("display","block");
+		$("#number-correct-answers").text("Correct Answers: " + gameLogic.correctGuesses)
+		$("#number-wrong-answers").text("Wrong Answers: " + gameLogic.wrongGuesses)
 	},
 
 	closeAllModals: function(){
@@ -239,6 +192,7 @@ var view = {
 		$("#incorrect-answer-screen").css("display","none");
 		$("#time-runout-screen").css("display","none");
 		$("#game-complete-screen").css("display","none");
+		$("#click-to-start").css("display","none");
 	},
 
 	//pull questions randomly out of dictionary keys and answers out of the list values
@@ -268,59 +222,63 @@ var view = {
 		//the correct answer will be the = to answer array index [0]
 			//the button dislays are random
 	//when question is answered incorrectly store +1 to incorrect answers
-function getAnswer (){
 
-	var rightAnswer = gameLogic.questionBank[gameLogic.currentQuestion].answers[0];//gameLogic.currentQuestion
+var handler = {
 
-	$(".answer-button").click(function(){
+	answerClicked: false,
 
-		var clickedAnswer = $(this).text();
-		console.log(clickedAnswer);
+	getAnswer: function  (){
 
-		// gameLogic.isQuestion = false;//this should set the inbetween timer
-		gameLogic.answered = true;
+		var rightAnswer = gameLogic.questionBank[gameLogic.currentQuestion].answers[0];//gameLogic.currentQuestion
 
-		if (clickedAnswer === rightAnswer){
-			//correct answer screen and timer
-			gameLogic.questionAnswerTimer();
-			view.correctAnswerScreen();
-		} else if(clickedAnswer !== rightAnswer){
-			//wrong answer screen and timer;
-			gameLogic.questionAnswerTimer();
-			view.wrongAnswerScreen();
-		}
+		$(".answer-button").click(function(){
 
-	});
+			if (handler.answerClicked === false){
 
+				var clickedAnswer = $(this).text();
+				console.log(clickedAnswer);
+				
+				//this should set the inbetween timer
+				gameLogic.answered = true;
+				handler.answerClicked = true;
+
+				if (clickedAnswer === rightAnswer){
+					//correct answer screen and timer
+					gameLogic.correctGuesses += 1;
+					gameLogic.timerDisplay = 5;
+					gameLogic.questionAnswerTimer();
+					view.correctAnswerScreen();
+				} else if(clickedAnswer !== rightAnswer){
+					//wrong answer screen and timer;
+					gameLogic.wrongGuesses += 1;
+					gameLogic.timerDisplay = 5;
+					gameLogic.questionAnswerTimer();
+					view.wrongAnswerScreen();
+				}
+			}
+
+		});
+
+	},
+
+	restart: function(){
+
+		$("#restart-game").click(function(){
+
+			handler.answerClicked = false;
+			gameLogic.currentQuestion = 0;
+			gameLogic.correctGuesses = 0;
+			gameLogic.wrongGuesses = 0;
+			view.closeAllModals();
+			view.clickToStartScreen();
+			questionsLoaded = false
+
+		})
+
+
+	}
 }
 
-
-//set up screen for correct answer
-//set up screen for incorrect answer
-//set up screen for time running out
-	//needs to cover up main answer div
-	// jquery hide and show methods will probably work here
-	//possibly other hidden divs that cover up the main one
-	//could use position relative an z score to set up
-
-
-
-
-//instructions for building advanced game
-// * You'll create a trivia game that shows only one question until the player
-// answers it or their time runs out.
-
-// * If the player selects the correct answer, show a screen congratulating them
-// for choosing the right option. After a few seconds, display the next question
-// -- do this without user input.
-
-// * The scenario is similar for wrong answers and time-outs.
-
-//   * If the player runs out of time, tell the player that time's up and
-// display the correct answer. Wait a few seconds, then show the next question.
-//   * If the player chooses the wrong answer, tell the player they selected the
-// wrong option and then display the correct answer. Wait a few seconds, then show
-// the next question.
-
-// * On the final screen, show the number of correct answers, incorrect answers,
-// and an option to restart the game (without reloading the page).
+view.clickToStartScreen();
+handler.getAnswer();
+handler.restart();
